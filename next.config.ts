23043c1fc79a4ce44b510e75fffdddc04eb3bcfd
kicky1618 <69379510+Kicky1618@users.noreply.github.com>
2026-07-25 00:47:07 +0900

    Refactor code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/kond-lang",
   reactStrictMode: true,
   output: "export",
-  images: { unoptimized: true },
 };
 
 export default nextConfig;
